574c7943abb57eab8f433bd91a88a7fa0a3e96e9
ggkk0818 <ggkk0818@sina.com>
2017-11-16 11:44:29 +0800

    修复拼写错误

--- space-news.user.js
+++ space-news.user.js
@@ -4,15 +4,15 @@
 // @version      1.0
 // @description  浏览联众星际家园新闻自动跳转到芬腾官网相应链接
 // @author       ggkk0818
-// @match        http://newsapce.ourgame.com/View.aspx?id=*
+// @match        http://newspace.ourgame.com/View.aspx?id=*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var regex = /http:\/\/newsapce\.ourgame\.com\/View\.aspx\?id=(\d+)/,
+    var regex = /http:\/\/newspace\.ourgame\.com\/View\.aspx\?id=(\d+)/,
         result = regex.exec(window.location.href);
     if(result && result.length){
-        window.location.href = "http://newsapce.sxfenteng.com/View.aspx?id=" + result[1];
+        window.location.href = "http://newspace.sxfenteng.com/View.aspx?id=" + result[1];
     }
 })();
